be415fd7fcddb73d68eea76e8d185a8ccd7c6808
moldybread322 <146388462+moldybread322@users.noreply.github.com>
2024-05-14 22:15:01 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function calculateBoxes() {
+    let stickers = parseInt(document.getElementById("stickers").value);
+    let stickersPerBox = 6;
+    let boxes = Math.floor(stickers / stickersPerBox);
+
+    let outputText;
+    if (boxes > 0) {
+        outputText = "You have " + boxes + " full boxes";
+    } else {
+        outputText = "You have no full boxes";
+    }
+
+    if (stickers >= 18 && stickers <= 20) {
+        outputText += "<br>You have a small box";
+    } else if (stickers >= 21 && stickers <= 30) {
+        outputText += "<br>You have a medium box";
+    }
+
+    document.getElementById("output").innerHTML = outputText;
+}
+
+hello
